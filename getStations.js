6c73b1a40c96a891c8123b44ab5415522608b90d
Michael Pesek <pesekm@dupage.edu>
2020-10-05 16:01:37 -0500

    to click link inside iframe: need to use frame.contentDocument not window

--- getStations.js
+++ getStations.js
@@ -8,9 +8,9 @@ const escapeXpathString = str => {
 
 const clickByText = async function(page, text, element) {
     
-    let SELECTOR = "#primary > main > article > div > p:nth-child(3) > span:nth-child(2) > a"
-    await page.focus(SELECTOR);
-    await page.waitFor(2000);
+    let SELECTOR = "#menu-item-2552 > a" 
+  
+    await page.waitForNavigation()
     await page.click(SELECTOR);
     await page.screenshot({
     path: './screenshots/pagel.png'
@@ -18,24 +18,36 @@ const clickByText = async function(page, text, element) {
 }
 
 
-console.log("test")
+
 
 startScript = async () => {
     try{
-        console.log("test")
+        
         const browser = await puppeteer.launch()
      
         const page = await browser.newPage()
      
 
-        await page.goto('http://www.radiosure.com/downloadz/')
+        await page.goto('http://www.radiosure.com/stations/') 
+
+        let linkSelector = "body > b > b:nth-child(26) > a" 
+
+        console.log("test", linkSelector)
+        
+        await page.evaluate((linkSelector) => {
+            
+
+            var frame = document.querySelector("#post-264 > div > p:nth-child(2) > iframe")
+            
+            var frameDocument = frame.contentDocument;
+    
+            frameDocument.querySelector(linkSelector).click();
+    
+        }, linkSelector);
+
       
 
-        await clickByText(page, "published stations")
-        await page.waitForNavigation({waitUntil: 'load'})
-
         
-
         await browser.close()
     } catch (error){
         console.log(error)
